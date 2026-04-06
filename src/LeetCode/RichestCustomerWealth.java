05b3a71a9e846751fd26834f6a3b54e869af5b37
Kaveen-Ambegoda <kaveenambegoda02@gmail.com>
2026-04-06 23:52:45 +0530

    RichestCustomerWealth(new updated code for LeetCode question)

--- src/LeetCode/RichestCustomerWealth.java
+++ src/LeetCode/RichestCustomerWealth.java
@@ -1,49 +1,29 @@
 package LeetCode;
 
-import java.util.Scanner;
-
 public class RichestCustomerWealth {
-    public static void main(String[] args){
-
-        Scanner sc = new Scanner(System.in);
-
-        int m, n;
-
-        System.out.print("Enter number of customers (rows): ");
-        m = sc.nextInt();
-
-        System.out.print("Enter number of banks (columns): ");
-        n = sc.nextInt();
-
-        int[][] accounts = new int[m][n];
+    public int maximumWealth(int[][] accounts) {
 
 
-        for (int i = 0; i < m; i++) {
-            System.out.println("Enter values for customer " + i + ":");
-            for (int j = 0; j < n; j++) {
-                System.out.print("Bank " + j + ": ");
-                accounts[i][j] = sc.nextInt();
+        int m = accounts.length;
+        int n = accounts[0].length;
+        int[] wealthCustomer = new int[m];
+        int max=0;
+
+        for(int i = 0; i<m ; i++){
+
+
+            for(int j = 0;j<n;j++){
+                wealthCustomer[i] = accounts[i][j]+wealthCustomer[i];
             }
+
+            if(wealthCustomer[i]>max){
+                max = wealthCustomer[i];
+            }
+
         }
 
-        int maxWealth = 0;
+    return max;
 
-
-        for (int i = 0; i < accounts.length; i++) {
-
-            int currentWealth = 0;
-
-            for (int j = 0; j < accounts[i].length; j++) {
-                currentWealth += accounts[i][j];
-            }
-
-            if (currentWealth > maxWealth) {
-                maxWealth = currentWealth;
-            }
-        }
-
-        System.out.println("Richest Customer Wealth: " + maxWealth);
-
-        sc.close();
     }
+
 }
